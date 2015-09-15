820c267f8aefca37041c2f9fb751e0a745a08383
School <School@Charless-MacBook.local>
2015-09-15 09:43:03 -0700

    Initial Commit

---
+++ ios_designers_wk1_challenge_1_google/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_designers_wk1_challenge_1_google
+//
+//  Created by School on 9/15/15.
+//  Copyright (c) 2015 School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
