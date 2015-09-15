19b481ef4997f850175eb1f12546f2d3c94e08a5
chieger <charlie@codepath.com>
2015-09-15 12:57:19 -0700

    iOS designers week 1 challenge 1 google
    CODEPATH iOS for Designers Challenge 1 google.
    
    Practice
    
    Adding:
    ViewControllers
    Buttons
    Segues: modal and Push
    Swift ViewController File
    
    Linking:
    ImageView with Image
    Swift ViewController File with StoryBoard View Controller
    
    Code:
    DismissViewControllerAnimated(true, nil)

---
+++ ios_designers_wk1_challenge_1_google/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  ios_designers_wk1_challenge_1_google
+//
+//  Created by School on 9/15/15.
+//  Copyright (c) 2015 School. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onDone(sender: AnyObject) {
+    // Do stuff when the button is clicked
+     dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
